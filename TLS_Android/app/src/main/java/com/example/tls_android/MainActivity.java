6acfe6aa1268714b18957079dfa113b3b9aafa66
raynnersc <raynnerschnneider@hotmail.com>
2024-06-10 15:13:14 +0200

    feat: connect and disconnect buttons are executable, logbox to show received data, textbox and send button to send data

--- TLS_Android/app/src/main/java/com/example/tls_android/MainActivity.java
+++ TLS_Android/app/src/main/java/com/example/tls_android/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.tls_android;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -19,6 +26,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -29,7 +37,12 @@ import com.example.tls_android.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,8 +50,9 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
     private ActivityResultLauncher<Intent> enableBluetoothLauncher;
+    private ActivityResultLauncher<Intent> discoverBluetoothLauncher;
 
-    Button btnTurnOn ;
+    @SuppressLint("MissingPermission")
     @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,27 +76,58 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
-
-        /*btnTurnOn.setOnClickListener(v -> {
-            Intent intent = new Intent();
-            intent.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
-            startActivity(intent);
-        });*/
-
-        btnTurnOn = findViewById(R.id.button_config_bluetooth);
+        Button btnTurnOn = findViewById(R.id.button_turnon_bluetooth);
         // Set the click listener for the button
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         btnTurnOn.setOnClickListener(v -> {
-            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
             if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled()) {
-                    // Permission has already been granted, proceed with enabling Bluetooth
-                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    enableBluetoothLauncher.launch(enableBtIntent);
+                // Permission has already been granted, proceed with enabling Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                enableBluetoothLauncher.launch(enableBtIntent);
+            }
+        });
+
+        Button btnConnect = findViewById(R.id.button_connect_bluetooth);
+        btnConnect.setOnClickListener(v -> {
+            Intent intentOpenBluetoothSettings = new Intent(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+            startActivity(intentOpenBluetoothSettings);
+        });
+
+        Button btnDisconnect = findViewById(R.id.button_disconnect_bluetooth);
+        btnDisconnect.setOnClickListener(v -> {
+            if (bluetoothAdapter != null && bluetoothAdapter.isEnabled()) {
+                bluetoothAdapter.disable();
+                Toast.makeText(MainActivity.this, "Bluetooth is now disabled", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView textLog = findViewById(R.id.text_receive_data);
+        Button btnSend = findViewById(R.id.button_send_data);
+        EditText editText = findViewById(R.id.text_send_data);
+
+        btnSend.setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED) {
+                if (bluetoothAdapter != null && bluetoothAdapter.isEnabled() && bluetoothAdapter.getProfileConnectionState(BluetoothProfile.STATE_CONNECTED) == BluetoothProfile.STATE_CONNECTED) {
+                    String text = editText.getText().toString();
+                    if (!text.isEmpty()) {
+                        String currentLog = textLog.getText().toString();
+                        String updatedLog = currentLog + "\n" + text;
+                        textLog.setText(updatedLog);
+                        editText.setText("");  // Clear the input field
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Not connected to any Bluetooth device", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.BLUETOOTH}, REQUEST_ENABLE_BT);
             }
         });
     }
+
     // Constant for request code
     private static final int REQUEST_ENABLE_BT = 1;
     private static final int REQUEST_BLUETOOTH_CONNECT = 2;
+    private static final int REQUEST_DISCOVER_BT = 3;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -119,10 +164,28 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_BLUETOOTH_CONNECT) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission was granted, try to open Bluetooth settings again
-                Button btnTurnOn = findViewById(R.id.button_config_bluetooth);
+                Button btnTurnOn = findViewById(R.id.button_turnon_bluetooth);
                 btnTurnOn.performClick();  // Re-attempt enabling Bluetooth
             } else {
                 // Permission was denied, handle the failure
+                Toast.makeText(this, "Bluetooth permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        else if (requestCode == REQUEST_ENABLE_BT) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Button btnSend = findViewById(R.id.button_send_data);
+                btnSend.performClick();
+            } else {
+                Toast.makeText(this, "Bluetooth permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        else if (requestCode == REQUEST_DISCOVER_BT) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Button btnConnect = findViewById(R.id.button_connect_bluetooth);
+                btnConnect.performClick();
+            } else {
                 Toast.makeText(this, "Bluetooth permission denied", Toast.LENGTH_SHORT).show();
             }
         }
